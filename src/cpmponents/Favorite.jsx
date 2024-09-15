1544c114352dbc78c48b8fb5013ed5d03e65cca6
Dense-lin <denselinm@gmail.com>
2024-09-15 18:12:19 +0800

    更新img位置

--- src/cpmponents/Favorite.jsx
+++ src/cpmponents/Favorite.jsx
@@ -12,7 +12,7 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="books" src="src\img\book.jpg" />}
+          cover={<img alt="books" src="public\img\book.jpg" />}
           
         >
           <Meta title="书籍" description="我看过的+我喜欢的" />
@@ -22,7 +22,7 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="music" src="src\img\music.jpg" />}
+          cover={<img alt="music" src="public\img\music.jpg" />}
         >
           <Meta title="音乐" description="我之所爱，动听之处" />
           <p>令我感动轻松的Music</p>
@@ -31,7 +31,7 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="movies" src="src\img\movie.jpg" />}
+          cover={<img alt="movies" src="public\img\movie.jpg" />}
         >
           <Meta title="电影" description="我所看过的电影都会一一记录分享" />
           <p>令我感同身受的Movies</p>
@@ -40,8 +40,8 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="CasualEssay" src="src\img\CasualEssay.jpg" />}
-          onClick={() => window.location.href = 'http://localhost:3000/#/'}
+          cover={<img alt="CasualEssay" src="public\img\CasualEssay.jpg" />}
+          onClick={() => window.open('https://dense-lin.github.io/Denselin-docsify/#/')}
         >
           <Meta title="我的随写" description="纯属文字爱好，并写下某一时间的我的感受与想法" />
           <p>平时的一些简单感受</p>
@@ -50,7 +50,7 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="Work" src="src\img\work.jpg" />}
+          cover={<img alt="Work" src="public\img\work.jpg" />}
         >
           <Meta title="我的作品" description="一些早期/开发中/实验性的作品" />
           <p>秃头也做不出</p>
@@ -59,7 +59,7 @@ const Favorite = () => (
       <Col span={8}>
         <Card
           hoverable
-          cover={<img alt="example" src="src\img\chendu.jpg" />}
+          cover={<img alt="example" src="public\img\chendu.jpg" />}
           onClick={() => window.open('https://www.douyin.com/user/self?from_tab_name=main')}
         >
           <Meta title="我的视频作品" description="在线观看我的原创视频作品" />
